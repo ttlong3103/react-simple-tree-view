aca7130ca12672bc102b9837c979105132012bfb
tran thanh long <ttlong3103>
2019-10-05 16:12:26 +0700

    provide icons in mock data

--- src/index.js
+++ src/index.js
@@ -1,40 +1,64 @@
 import React, { Component } from 'react';
 import TreeView from './TreeView';
+import Folder from './icon/folder.svg'
+import File from './icon/file.svg'
+
+const styleOfFile = { marginLeft: '0px', marginRight: '5px' };
+const styleOfFolder = { marginLeft: '5px' };
+
+
+function FolderIcon () {
+  return <img src={Folder} width={18} height={18} />;
+};
+function FileIcon () {
+  return <img src={File} width={15} height={15} />;
+};
+
 
 const treeData = [
   {
+    icon: <FolderIcon />,
     title: 'folder1',
     childNodes: ['readme.md', 'test.js'].map((x) => ({
+      icon: <FileIcon />,
       title: x,
     })),
   },
   {
+    icon: <FolderIcon />,
     title: 'folder2',
     childNodes: ['a.css', 'b.js', 'c.txt'].map((x) => ({
+      icon: <FileIcon />,
       title: x,
     })),
     isExpanded: true,
   },
   {
+    icon: <FileIcon />,
     title: 'index.js',
   },
   {
+    icon: <FolderIcon />,
     title: 'folder3',
     childNodes: [
       {
+        icon: <FileIcon />,
         title: 'index.js',
       },
       {
+        icon: <FolderIcon />,
         title: 'components',
         // isExpanded: true,
         childNodes: [
           {
+            icon: <FileIcon />,
             title: 'abc',
           },
           {
+            icon: <FolderIcon />,
             title: 'xyz',
             isExpanded: true,
-            childNodes: ['1', '2', '3'].map((x) => ({ title: x })),
+            childNodes: ['1', '2', '3'].map((x) => ({ icon: <FileIcon />, title: x })),
           },
         ],
       },
