687591b5d328a79562bada50218e40dcc0ece2c3
tran thanh long <ttlong3103>
2019-11-16 16:49:52 +0700

    move example code to correct folder

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,102 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'react-simple-tree-view'
+import TreeView from 'react-simple-tree-view';
+
+const treeData = [
+	{
+		icon: <FolderIcon />,
+		title:
+			'This is a folder that has really really really really really really long name',
+		childrenNode: ['README.md', '_test_.js'].map((x) => ({
+			icon: <FileIcon />,
+			title: x,
+		})),
+	},
+	{
+		icon: <FolderIcon />,
+		title: 'folder A',
+		childrenNode: ['a.css', 'b.js', 'c.txt'].map((x) => ({
+			icon: <FileIcon />,
+			title: x,
+		})),
+		isExpanded: true,
+	},
+	{
+		icon: <FileIcon />,
+		title: 'index.js',
+	},
+	{
+		icon: <FolderIcon />,
+		title: 'folder B',
+		childrenNode: [
+			{
+				icon: <FileIcon />,
+				title: 'index.js',
+			},
+			{
+				icon: <FolderIcon />,
+				title: 'components',
+				// isExpanded: true,
+				childrenNode: [
+					{
+						icon: <FileIcon />,
+						title: 'Button.js',
+					},
+					{
+						icon: <FolderIcon />,
+						title: 'TextInput',
+						isExpanded: true,
+						childrenNode: [
+							'InputNumber.js',
+							'InputDate.js',
+							'Input.js',
+						].map((x) => ({ icon: <FileIcon />, title: x })),
+					},
+				],
+			},
+		],
+		isExpanded: true,
+	},
+];
 
 export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
-  }
+	state = {
+		history: '',
+	};
+	addHistory = (text) => {
+		this.setState((state) => {
+			return {
+				history: state.history + +'- ' + text + '\n',
+			};
+		});
+	};
+	onExpandNode = ({ node }, updatedTree) => {
+		this.addHistory('You expanded ' + node.title);
+	};
+
+	onCollapseNode = ({ node }, updatedTree) => {
+		this.addHistory('You collapsed ' + node.title);
+	};
+
+	onClickNode = (e, { node }, updatedTree) => {
+		this.addHistory('You clicked ' + node.title);
+	};
+	render() {
+		return (
+			<div style={{ display: 'flex' }}>
+				<TreeView
+					initialData={treeData}
+					onExpandNode={this.onExpandNode}
+					onCollapseNode={this.onCollapseNode}
+					onClickNode={this.onClickNode}
+					style={{
+						backgroundColor: '#e5e5e5',
+						width: 300,
+						height: '100%',
+					}}
+				/>
+				<div style={{ margin: 20 }}>{this.state.history}</div>
+			</div>
+		);
+	}
 }
